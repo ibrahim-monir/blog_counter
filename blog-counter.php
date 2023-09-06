25d89a9ea3dace755449e6512de202a3b3e228fb
ibrahim-monir <ibrahimkhalilmp@gmail.com>
2023-09-06 11:24:36 +0600

    version_1.0.0 release

---
+++ blog-counter.php
@@ -0,0 +1,44 @@
+<?php 
+/*
+* Plugin Name: Blog Counter
+* Plugin URI: test.com
+* Description: This is Blog post counter plugin
+* Version: 1.0.0
+* Author: Ibrahim Monir
+* Author URI: khalil.com
+* License: GPL v2 or letter
+* Text Domain: blog-counter
+
+*/
+
+add_shortcode( 'blog_counter', 'blog_counter_func' );
+
+function blog_counter_func(){
+  
+    $arg = array(
+        'post_type' => 'post',
+        'posts_per_page' => -1,
+    );
+
+    $query = new WP_Query($arg);
+    ob_start();
+    if($query->have_posts()):
+    ?>
+    <ul>
+        <?php
+            while($query->have_posts()){
+                $query->the_post();
+                echo '<li><a href="'.get_the_permalink().'">'.get_the_title().'</a></li>';
+            }
+        
+        ?>
+    </ul>
+
+    <?php
+    endif;
+
+    $html = ob_get_clean();
+
+    return $html;
+     
+}
